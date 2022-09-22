6a6027a7e4f51f3c72d1918498c841774405d8ed
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2022-09-22 13:24:56 -0500

    committing lines in js of vanilla js and jQuery

---
+++ script.js
@@ -0,0 +1,77 @@
+
+//document.querySelector('h1'); We are spesifiing the css selector to be manipulated
+$('h1');
+
+//document.querySelectorAll('button');
+$('button');
+
+//document.querySelextor('h1').style.color = 'green'; is the same as what follows with the use of a css method where a single property in the parenthesis gets the valude but if you pass a second propertiy you are setting the value. 
+$('h1').css('color', 'green');
+
+
+//due to seperation of concerns, we dont want to manipulate css in js file but rather add a class in the css and toggle it on or off as needed within the jsn such as this:  document.querySelector('h1').classList.add('big-title');
+$('h1').addClass('big-title');
+
+// you can remove the class which would be written in js as: doxument.querySelector('h1').classList.remove('big-title');
+$('h1').removeClass('big-title');
+
+
+// you can add or remove multiple xalsses with the following the class which would be written in js as: document.querySelector('h1').classList.add('big-title');
+$('h1').addClass('big-title margin-50');
+
+//you can also query to see if the selector has a class which returns a boolean example:
+console.log($('h1').hasClass('margin-50'));
+
+// change text of html  document.querySelector('h1').innerHTML = new text
+$('h1').text('new text');
+
+// to implement new text with html elements 
+$('button').html('<em>Display new text</em>');
+
+//change attribute value for documement.querySelector('img').setAttrivute('src','./path/image.png');
+// $('img').attr('src', 'image.png');
+// console.log($('img').attr('src'));
+
+// manipulate classes sing attriute
+//document.querySelector('h2').setAttribute('class', 'other');
+$('h2').attr('class', 'other');
+
+// document.querySelector('h2').addEventListener('click', function(){
+//     console.log('I was clicked');
+// });
+$('h2').click(function () {
+    console.log('jQuery clicked me');
+});
+
+// for (let i = 0; i < 5; i++){
+//     document.querySelectorAll('button')[i].addEventListener('click', function(){
+//         document.querySelector('h3').style.color = 'purple';
+//     });
+// }
+
+$('button').click(function(){
+    $('h3').css('color', 'purple');
+})
+ //to add event listeners and console log the event key we did
+// document.addEventListener('keydown', function (event) {
+//   console.log(event.key);
+// });
+
+$(document).keypress(function(event){
+    console.log(event.key);
+});
+$(document).keypress(function(event){
+    $('h1').text(event.key);
+});
+//or
+$(document).on('click', function (){
+    $('h1').css('color', 'purple');
+});
+
+// add elements on the fly. You also have options of .after, .prepend, .append. Worth noting prepend and append add the content within the element indicated. 
+$('h1').before('<button>New Button</button>');
+
+//remove all of the indicated element with:
+$('button').remove();
+
+
